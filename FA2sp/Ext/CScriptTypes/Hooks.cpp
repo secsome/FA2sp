3190741a5a52a88324c3fd3ccd12e6bd71330712
secsome <302702960@qq.com>
2020-09-22 23:57:24 +0800

    remake CScriptTypes::OnInitDialog

--- FA2sp/Ext/CScriptTypes/Hooks.cpp
+++ FA2sp/Ext/CScriptTypes/Hooks.cpp
@@ -4,32 +4,29 @@
 
 
 // Read from ScriptsRA2 in FAData
-//DEFINE_HOOK(4D8020, CScriptTypes_OnInitDialog, 6)
-//{
-//    INIClass* pFAData = &GlobalVars::INIFiles::FAData.get();
-//
-//    // idx begins from 0
-//    constexpr int ScriptCountRA2 = 59;
-//    constexpr int ScriptCountYR = 65;
-//    
-//    const bool& bSupportYR = *reinterpret_cast<bool*>(0x5D32AC);
-//
-//    const int nScriptCount = pFAData->GetInteger("ScriptsRA2", "ScriptCount", bSupportYR ? ScriptCountYR : ScriptCountRA2);
-//    
-//
-//
-//    pFAData->GetInteger("ScriptsRA2", "ScriptsCount", 64);
-//    
-//
-//
-//    return 0x4D8E06;
-//}
-
-DEFINE_HOOK(4D8D3C, test__, 7)
+DEFINE_HOOK(4D8020, CScriptTypes_OnInitDialog, 6)
 {
     GET(CScriptTypes*, pThis, ECX);
-    HWND hwndThis = pThis->m_hWnd;
-    HWND hwndTest = pThis->GetDlgItem(1919)->m_hWnd;
-    Logger::Debug("this = %08X, test = %08X", hwndThis, hwndTest);
-    return 0;
+    
+    pThis->OnInitDialog();
+
+    while (pThis->CCBCurrentAction.DeleteString(0) != -1)
+        continue;
+
+    constexpr int FA2ActionNamesArraySize = 59;
+    const char** pFA2ActionNamesArray = reinterpret_cast<const char**>(0x5D035C);
+    for (int i = 0; i < FA2ActionNamesArraySize; ++i)
+        int idx = pThis->CCBCurrentAction.AddString(pFA2ActionNamesArray[i]);
+
+    return 0x4D8E06;
 }
+
+
+//DEFINE_HOOK(4D6A10, CScriptTypes_OnParamConvert, 7)
+//{
+//    GET(CScriptTypes*, pThis, ECX);
+//
+//
+//
+//    return 0x4D7569;
+//}
