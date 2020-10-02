5a8986c861f3116945ce11bf6b8e07404df1805d
secsome <302702960@qq.com>
2020-10-03 00:51:25 +0800

    upd updcodes

--- update_codes.bat
+++ update_codes.bat
@@ -2,6 +2,13 @@ git init
 git remote add origin https://github.com/secsome/FA2sp.git
 git fetch
 git checkout master
+mkdir VC5
+cd VC5
+git init
+git remote add origin https://github.com/secsome/VC5.git
+git fetch
+git checkout main
+cd ..
 mkdir MFC42
 cd MFC42
 git init
