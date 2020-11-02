a156caac1201947e532949bec99c6fecd79f6cbc
secsome <302702960@qq.com>
2020-11-03 00:27:58 +0800

    daily commit

--- FA2sp/Utils/Misc/Hooks.BugFixes.cpp
+++ FA2sp/Utils/Misc/Hooks.BugFixes.cpp
@@ -1,5 +1,7 @@
 #include <Helpers/Macro.h>
 
+#include "../../Logger.h"
+
 // FA2 will no longer automatically change the extension of map
 DEFINE_HOOK(42703A, FA2Main_SaveMap_Extension, 9)
 {
@@ -19,3 +21,12 @@ DEFINE_HOOK(41FDDB, FA2Main_GetFilePath_2, 6)
 }
 
 
+////Make cliff tools work in lunar as well
+//DEFINE_HOOK(435370, FA2Main_LunarCliff_Debug, 6)
+//{
+//	GET_STACK(DWORD, dwCallerAddress, 0x0);
+//	Logger::Debug(__FUNCTION__" Caller = %P\n", dwCallerAddress);
+//	return 0;
+//}
+//
+//DEFINE_HOOK_AGAIN(4352F0, FA2Main_LunarCliff_Debug, 6);
