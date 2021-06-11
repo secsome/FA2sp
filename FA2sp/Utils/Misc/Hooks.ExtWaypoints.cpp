b6c6dffcd899162e6466dd2385b87436cffd494b
secsome <302702960@qq.com>
2021-06-11 21:04:56 +0800

    also for TransportWaypoint

--- FA2sp/Utils/Misc/Hooks.ExtWaypoints.cpp
+++ FA2sp/Utils/Misc/Hooks.ExtWaypoints.cpp
@@ -7,22 +7,52 @@
 
 #include <string>
 
+class ExtWaypoint
+{
+public:
+	static int String_To_Waypoint(const char* s)
+	{
+		int n = 0;
+		int len = strlen(s);
+		for (int i = len - 1, j = 1; i >= 0; i--, j *= 26)
+		{
+			int c = toupper(s[i]);
+			if (c < 'A' || c > 'Z') return 0;
+			n += ((int)c - 64) * j;
+		}
+		if (n <= 0)
+			return -1;
+		return n - 1;
+	}
+
+	static std::string Waypoint_To_String(int n)
+	{
+		if (n < 0)
+			return "0";
+		else
+		{
+			std::string buffer;
+			++n;
+			while (n> 0)
+			{
+				int m = n% 26;
+				if (m == 0) m = 26;
+				buffer = (char)(m + 64) + buffer;
+				n = (n- m) / 26;
+			}
+			return buffer;
+		}
+	}
+};
+
 DEFINE_HOOK(4E5F30, String_To_Waypoint, 5)
 {
     if (!ExtConfigs::ExtWaypoints)
         return 0;
 
-	GET_STACK(char*, pString, 0x4);
+	GET_STACK(const char*, pString, 0x4);
 
-	int n = 0;
-	int len = strlen(pString);
-	for (int i = len - 1, j = 1; i >= 0; i--, j *= 26)
-	{
-		int c = toupper(pString[i]);
-		if (c < 'A' || c > 'Z') return 0;
-		n += ((int)c - 64) * j;
-	}
-	R->EAX(n - 1);
+	R->EAX(ExtWaypoint::String_To_Waypoint(pString));
 
 	return 0x4E5F87;
 }
@@ -35,31 +65,14 @@ DEFINE_HOOK(4E5F90, Waypoint_To_String, 7)
 	GET_STACK(ppmfc::CString*, pString, 0x4);
 	GET_STACK(int, nWaypoint, 0x8);
 
-	if (nWaypoint == -1)
-		pString->Format("0");
-	else
-	{
-		std::string buffer;
-		
-		++nWaypoint;
-		while (nWaypoint > 0)
-		{
-			int m = nWaypoint % 26;
-			if (m == 0) m = 26;
-			buffer = (char)(m + 64) + buffer;
-			nWaypoint = (nWaypoint - m) / 26;
-		}
-		
-		// Call the CTOR
-		new(pString) ppmfc::CString(buffer.c_str());
-	}
+	new(pString) ppmfc::CString(ExtWaypoint::Waypoint_To_String(nWaypoint).c_str());
 
 	R->EAX(pString);
 
 	return 0x4E60FA;
 }
 
-DEFINE_HOOK(4E97F4, CTeamTypes_OnCBCurrentTeamSelectChanged_String_To_Waypoint, 5)
+DEFINE_HOOK(4E97F4, CTeamTypes_OnCBCurrentTeamSelectChanged_String_To_Waypoint_1, 5)
 {
 	if (!ExtConfigs::ExtWaypoints)
 		return 0;
@@ -67,16 +80,28 @@ DEFINE_HOOK(4E97F4, CTeamTypes_OnCBCurrentTeamSelectChanged_String_To_Waypoint, 
 	GET(CTeamTypes*, pThis, EBP);
 	REF_STACK(ppmfc::CString, lpWaypoint, STACK_OFFS(0x178, 0x160));
 
-	int n = 0;
-	int len = strlen(lpWaypoint);
-	for (int i = len - 1, j = 1; i >= 0; i--, j *= 26)
-	{
-		int c = toupper(lpWaypoint[i]);
-		if (c < 'A' || c > 'Z') return 0;
-		n += ((int)c - 64) * j;
-	}
-
-	pThis->CString_Waypoint.Format("%d", n - 1);
+	const int wp = ExtWaypoint::String_To_Waypoint(lpWaypoint);
+	if (wp != -1)
+		pThis->CString_Waypoint.Format("%d", wp);
+	else
+		pThis->CString_Waypoint = "";
 
 	return 0x4E9890;
+}
+
+DEFINE_HOOK(4E9959, CTeamTypes_OnCBCurrentTeamSelectChanged_String_To_Waypoint_2, 5)
+{
+	if (!ExtConfigs::ExtWaypoints)
+		return 0;
+
+	GET(CTeamTypes*, pThis, EBP);
+	REF_STACK(ppmfc::CString, lpWaypoint, STACK_OFFS(0x178, 0x128));
+
+	const int wp = ExtWaypoint::String_To_Waypoint(lpWaypoint);
+	if (wp != -1)
+		pThis->CString_TransportWaypoint.Format("%d", wp);
+	else
+		pThis->CString_TransportWaypoint = "None";
+
+	return 0x4E9A44;
 }
