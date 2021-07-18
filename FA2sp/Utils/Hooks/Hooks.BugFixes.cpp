a5ded25ff0961bea3929fead3ca677df550826fb
secsome <302702960@qq.com>
2021-07-18 16:42:53 +0800

    Fix the bug that Remap color cannot be drawn correctly

--- FA2sp/Utils/Hooks/Hooks.BugFixes.cpp
+++ FA2sp/Utils/Hooks/Hooks.BugFixes.cpp
@@ -1,6 +1,9 @@
 #include <Helpers/Macro.h>
 #include <GlobalVars.h>
+#include <CINI.h>
 #include <CMapData.h>
+#include <Drawing.h>
+#include <Palette.h>
 
 #include <MFC/ppmfc_cstring.h>
 
@@ -74,5 +77,30 @@ DEFINE_HOOK(483F9D, CLoading_LoadObjects_SHPTurretFix_2, 5)
     if (GlobalVars::INIFiles::Rules->GetBool(pRegName, "Turret") && bIsBuilding)
         return 0x483FA4;
     else
-        return 0x483FC2;
+        return 0;
+}
+
+// Fix bug for incorrect color while drawing
+DEFINE_HOOK(468760, Miscs_GetColor, 7)
+{
+	GET_STACK(const char*, pHouse, 0x4);
+	GET_STACK(const char*, pColor, 0x8);
+
+	ppmfc::CString color = "";
+	if (pHouse)
+		if (auto pStr = GlobalVars::INIFiles::CurrentDocument->TryGetString(pHouse, "Color"))
+			color = *pStr;
+
+	if (pColor)
+		color = pColor;
+
+	HSVClass hsv{ 0,0,0 };
+	if (!color.IsEmpty())
+		if (auto const ppValue = GlobalVars::INIFiles::Rules->TryGetString("Colors", color))
+			sscanf_s(*ppValue, "%hhu,%hhu,%hhu", &hsv.H, &hsv.S, &hsv.V);
+
+	RGBClass rgb = hsv;
+	R->EAX<int>(rgb);
+
+	return 0x468EEB;
 }
