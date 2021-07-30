dbde6e997dcf94b188762154e7f50f1dc4e9c54b
secsome <302702960@qq.com>
2021-07-31 02:21:10 +0800

    Fix wrong SafetyGuard limitation

--- FA2sp/Ext/CTaskforce/Hooks.cpp
+++ FA2sp/Ext/CTaskforce/Hooks.cpp
@@ -9,9 +9,9 @@ DEFINE_HOOK(4E41A0, CTaskforce_OnCBMemberTypeEditChanged, 6)
 	return pThis->CCBMemberType.GetWindowTextLength() ? 0 : 0x4E4539;
 }
 
-DEFINE_HOOK(4E48DB, CTaskforce_OnBNAddMemberClicked_SafeGuard, 6)
+DEFINE_HOOK(4E48DB, CTaskforce_OnBNAddMemberClicked_SafetyGuard, 6)
 {
 	GET(CTaskForce*, pThis, ESI);
 
-	return pThis->CLBMembers.GetCount() < 5 ? 0 : 0x4E48FC;
+	return pThis->CLBMembers.GetCount() < 6 ? 0 : 0x4E48FC;
 }
