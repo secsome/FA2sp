cb5e902f573f1a2d826965afa4dccc889db872a7
secsome <302702960@qq.com>
2021-09-23 20:26:11 +0800

    Support pal file not in cache.mix

---
+++ FA2sp/Ext/CLoading/Hooks.InitPalettes.cpp
@@ -0,0 +1,58 @@
+#include "Body.h"
+
+#include <CPalette.h>
+#include <FAMemory.h>
+
+DEFINE_HOOK(48B020, CLoading_InitPalettes, 7)
+{
+    GET(CLoadingExt*, pThis, ECX);
+
+    BytePalette::LoadedPaletteCount() = 0;
+
+    auto loadPalette = [&pThis](const char* pName, int& to) -> bool
+    {
+        if (BytePalette::LoadedPaletteCount() > 0xFF)
+            return false;
+
+        if (auto pBuffer = (BytePalette*)pThis->ReadWholeFile(pName))
+        {
+            for (int i = 0; i < 256; ++i)
+            {
+                pBuffer->Data[i].red <<= 2;
+                pBuffer->Data[i].green <<= 2;
+                pBuffer->Data[i].blue <<= 2;
+            }
+            to = ++BytePalette::LoadedPaletteCount();
+            BytePalette::LoadedPalettes[BytePalette::LoadedPaletteCount()] = *pBuffer;
+            BytePalette::LoadedPaletteFlag[BytePalette::LoadedPaletteCount()] = TRUE;
+            GameDelete(pBuffer);
+            return true;
+        }
+    };
+
+    loadPalette("isotem.pal", pThis->PAL_ISOTEM);
+    loadPalette("isosno.pal", pThis->PAL_ISOSNO);
+    loadPalette("isourb.pal", pThis->PAL_ISOURB);
+    loadPalette("isoubn.pal", pThis->PAL_ISOUBN);
+    loadPalette("isolun.pal", pThis->PAL_ISOLUN);
+    loadPalette("isodes.pal", pThis->PAL_ISODES);
+
+    loadPalette("unittem.pal", pThis->PAL_UNITTEM);
+    loadPalette("unitsno.pal", pThis->PAL_UNITSNO);
+    loadPalette("uniturb.pal", pThis->PAL_UNITURB);
+    loadPalette("unitubn.pal", pThis->PAL_UNITUBN);
+    loadPalette("unitlun.pal", pThis->PAL_UNITLUN);
+    loadPalette("unitdes.pal", pThis->PAL_UNITDES);
+
+    loadPalette("temperat.pal", pThis->PAL_TEMPERAT);
+    loadPalette("snow.pal", pThis->PAL_SNOW);
+    loadPalette("urban.pal", pThis->PAL_URBAN);
+    loadPalette("urbann.pal", pThis->PAL_URBANN);
+    loadPalette("lunar.pal", pThis->PAL_LUNAR);
+    loadPalette("desert.pal", pThis->PAL_DESERT);
+
+    if (!loadPalette("lib.pal", pThis->PAL_LIB_ID2124019542))
+        loadPalette("_ID2124019542", pThis->PAL_LIB_ID2124019542); // not quite understand why origin FA2 called this
+
+    return 0x48C3CD;
+}
