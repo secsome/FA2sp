56df28381ce2176ea8b6d9ac4d0f7cd56a545aeb
secsome <302702960@qq.com>
2021-11-11 15:06:45 +0800

    Now Property Brush can be handled in OnMouseMove

--- FA2sp/Miscs/Hooks.BugFixes.cpp
+++ FA2sp/Miscs/Hooks.BugFixes.cpp
@@ -117,7 +117,7 @@ DEFINE_HOOK(473E66, CIsoView_Draw_InfantrySubcell, B)
 DEFINE_HOOK(4C61C5, CMapData_ResizeMap_PositionFix, 5)
 {
 	GET(CellData*, pCell, EAX);
-	auto const pSrc = CONTAINING_RECORD(R->ECX(), CellData, LAT);
+	auto const pSrc = CONTAINING_RECORD(R->ECX(), CellData, Flag);
 
 	pCell->Smudge = pSrc->Smudge;
 	pCell->SmudgeType = pSrc->SmudgeType;
