b9ced7de7f1508fbfc62e9a685bd8d78070bd8d7
secsome <302702960@qq.com>
2021-11-27 23:32:46 +0800

    Fix a stupid bug

--- FA2sp/Miscs/Hooks.Mix.cpp
+++ FA2sp/Miscs/Hooks.Mix.cpp
@@ -10,12 +10,10 @@ DEFINE_HOOK(5281EE, MixFile_Open_CheckRAUnencrypted, 5)
 	return 0x52823E;
 }
 
-DEFINE_HOOK(527F95, MixFile_Open_CheckRAEncrypted, 6)
+DEFINE_HOOK(5280FC, MixFile_Open_CheckRAEncrypted, 7)
 {
 	return 0x528124;
 }
-
-int extramix = 0;
 
 #include "../FA2sp.h"
 #include <CFinalSunApp.h>
@@ -50,7 +48,7 @@ DEFINE_HOOK(48A1AD, CLoading_InitMixFiles_ExtraMix, 7)
 			if (auto id = CMixFile::Open(path, 0))
 			{
 				ExtraMixes.push_back(id);
-				CFA2Logger::Write("Successfully loaded extra mix file from %s\n", path);
+				CFA2Logger::WriteLine("Successfully loaded extra mix file from %s", path);
 			}
 		}
 	}
