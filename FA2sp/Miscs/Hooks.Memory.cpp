f0821cfcd569047ed0130a6ba9a5d510173f0314
secsome <302702960@qq.com>
2023-04-27 13:31:34 +0800

    Replace CurrentSHP buffer, now read from dir first

--- FA2sp/Miscs/Hooks.Memory.cpp
+++ FA2sp/Miscs/Hooks.Memory.cpp
@@ -25,6 +25,20 @@ DEFINE_HOOK(537128, ExeStart_MemoryHooks, 5)
 	RunTime::ResetMemoryContentAt(0x591154, ::HeapDestroy);
 	RunTime::ResetMemoryContentAt(0x591158, ::HeapCreate);
 
+	// RunTime::ResetMemoryContentAt(0x59128C, ::GlobalAlloc);
+	// RunTime::ResetMemoryContentAt(0x5912FC, ::GlobalFree);
+	// RunTime::ResetMemoryContentAt(0x591254, ::GlobalReAlloc);
+	// 
+	// RunTime::ResetMemoryContentAt(0x591270, ::LocalAlloc);
+	// RunTime::ResetMemoryContentAt(0x5912B8, ::LocalFree);
+	// RunTime::ResetMemoryContentAt(0x591248, ::LocalReAlloc);
+	// 
+	// RunTime::ResetMemoryContentAt(0x591268, ::TlsAlloc);
+	// RunTime::ResetMemoryContentAt(0x59125C, ::TlsFree);
+	// 
+	// RunTime::ResetMemoryContentAt(0x591160, ::VirtualAlloc);
+	// RunTime::ResetMemoryContentAt(0x59115C, ::VirtualFree);
+
 	RunTime::SetJump(0x536106, (DWORD)MemoryWrapper::_free);
 	RunTime::SetJump(0x537CA0, (DWORD)MemoryWrapper::_malloc);
 
--- FA2sp/Miscs/Hooks.Mix.cpp
+++ FA2sp/Miscs/Hooks.Mix.cpp
@@ -1,16 +1,16 @@
 #include <Helpers/Macro.h>
 
-DEFINE_HOOK(527F95, MixFile_Open_CheckTD, 7)
+DEFINE_HOOK(527F95, MixFile_PostOpen_CheckTD, 7)
 {
 	return 0x527FBB;
 }
 
-DEFINE_HOOK(5281EE, MixFile_Open_CheckRAUnencrypted, 5)
+DEFINE_HOOK(5281EE, MixFile_PostOpen_CheckRAUnencrypted, 5)
 {
 	return 0x52823E;
 }
 
-DEFINE_HOOK(5280FC, MixFile_Open_CheckRAEncrypted, 7)
+DEFINE_HOOK(5280FC, MixFile_PostOpen_CheckRAEncrypted, 7)
 {
 	return 0x528124;
 }
